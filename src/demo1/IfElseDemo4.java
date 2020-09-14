d38471318ada3dba8dbd36216b989f5b6816d1ed
CodeGym7 <swathibh16@gmail.com>
2020-09-14 11:20:53 +0200

    Adds a program to check a person be a blood donator

---
+++ src/demo1/IfElseDemo4.java
@@ -0,0 +1,20 @@
+package demo1;
+
+public class IfElseDemo4 {
+	
+	public static void main(String[] args) {
+		int age =20;
+		int weight=40;
+		
+		if(age>=20) {
+			if(weight>=50) {
+				System.out.println("you can donate blood");
+			}
+			else
+			{
+				System.out.println("You cannot donate blood");
+			}
+		}
+	}
+
+}
