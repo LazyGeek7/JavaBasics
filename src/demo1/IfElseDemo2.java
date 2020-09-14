795216daf2497117555898995f98b4bdfdb822ae
CodeGym7 <swathibh16@gmail.com>
2020-09-14 11:03:16 +0200

    Adds a program to check a number if its even or odd

---
+++ src/demo1/IfElseDemo2.java
@@ -0,0 +1,21 @@
+package demo1;
+
+/*Even or Odd*/
+
+public class IfElseDemo2 {
+
+	public static void main(String[] args) {
+		
+		int number=21;
+		
+		if(number%2==0) {
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+		
+	}
+
+}
