fb1ce791079ebb247f1837443580676e12515316
CodeGym7 <swathibh16@gmail.com>
2020-09-14 10:45:03 +0200

    Adds if else java program

---
+++ src/demo1/IfElseDemo.java
@@ -0,0 +1,18 @@
+package demo1;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		int number=20;
+		if(number>0) {
+			System.out.println("The number is positive");
+		}
+		else
+		{
+			System.out.println("The number is negative");
+		}
+
+	}
+
+}
