8854ffe3d5d9950ed6538255bfa6cb95516b8f30
CodeGym7 <swathibh16@gmail.com>
2020-09-14 11:25:33 +0200

    Adds a program to demo Switch case

---
+++ src/demo1/SwitchDemo1.java
@@ -0,0 +1,24 @@
+package demo1;
+
+public class SwitchDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number =100;
+		
+		switch(number) {
+		case 10 : System.out.println("The number is 10");
+		break;
+		case 20 : System.out.println("The number is 20");
+		break;
+		case 30 : System.out.println("The number is 30");
+		break;
+		case 40 : System.out.println("The number is 40");
+		break;
+		default : System.out.println("Not a valid number");
+		}
+
+	}
+
+}
