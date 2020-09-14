02e72de86cfbed9c06377172f80e39ea3af63863
CodeGym7 <swathibh16@gmail.com>
2020-09-14 11:16:23 +0200

    Adds a program to check leap year

---
+++ src/demo1/IfElseDemo3.java
@@ -0,0 +1,27 @@
+package demo1;
+
+public class IfElseDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=1988;
+		
+		if((year%4==0 && year%100!=0) || (year%400==0) ){
+			
+			 
+				System.out.println(" leap year");
+		}
+				else
+				{
+					
+			
+			
+			System.out.println("No leap year");	
+			
+		
+		
+
+	}
+
+}
+}
